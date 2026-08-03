d76f3dcd381e5389d9a19e1932df8211c96a6b32
CHINNAPAREDDY VENKATA MEGHANA REDDY <24bq1a0587@vvit.net>
2026-08-03 18:57:51 +0530

    Add LeetCode 1920 - Build Array from Permutation

---
+++ Arrays/1920-Build-Array-from-Permutation.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int ans[]=new int[nums.length];
+        int c=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
+}
