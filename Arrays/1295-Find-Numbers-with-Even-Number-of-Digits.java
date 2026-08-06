7530e8c11d550a4302dcd17ef89456d2624364ce
CHINNAPAREDDY VENKATA MEGHANA REDDY <24bq1a0587@vvit.net>
2026-08-06 22:39:20 +0530

    Solved LeetCode 1295 - Find Numbers with Even Number of Digits

--- Arrays/1295-Find-Numbers-with-Even-Number-of-Digits.java
+++ Arrays/1295-Find-Numbers-with-Even-Number-of-Digits.java
@@ -2,7 +2,7 @@ class Solution {
     public int findNumbers(int[] nums) {
         int check=0;
         for(int i=0;i<nums.length;i++)
-        {
+            {
             int c=0;
             while(nums[i]>0)
             {
